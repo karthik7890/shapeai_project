37c4dc08ecd16be527b971b341b74e74ae3b1cd5
Gorantla karthik <75745272+karthik7890@users.noreply.github.com>
2021-06-21 22:54:04 +0530

    Create weather_report.py

---
+++ .github/workflows/weather_report.py
@@ -0,0 +1,30 @@
+  
+import requests
+#import os
+from datetime import datetime
+
+api_key = '87d845b0b6cf29baa1a73cc34b067a95'
+location = input("Enter the city name: ")
+
+f=open("report.txt","w")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+f.write ("-------------------------------------------------------------")
+f.write ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+f.write ("-------------------------------------------------------------")
+
+f.write("Current temperature is: {:.2f} deg C".format(temp_city))
+f.write("Current weather desc  :",weather_desc)
+f.write("Current Humidity      :",hmdt, '%')
+f.close()
+print ("Current wind speed    :",wind_spd ,'kmph')
